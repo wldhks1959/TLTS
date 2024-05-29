c83719901d160f2907979e097ab5f55dab2ddeb0
m0v3m1n <east.sky.l33@gmail.com>
2024-05-29 18:43:46 +0900

    CREATE_TABLE 수정 및 HOBBY_INIT 수정.

--- public/data/db_sql/CREATE_TABLE.sql
+++ public/data/db_sql/CREATE_TABLE.sql
@@ -12,13 +12,20 @@ create table hobbies
 (
 	hobby_id char(20) primary key not null, -- 취미 이름
 	hobby_place char(20), -- 취미 즐길 장소
-	I_O ENUM ('indoor', 'outdoor'), -- 실내, 실외
-	S_M ENUM ('solo', 'multi'), -- 혼자/여럿
-	P_W ENUM ('play', 'watch'), -- 직접/관람
+	I_O ENUM ('indoor', 'outdoor', 'ANY'), -- 실내, 실외
+	S_M ENUM ('solo', 'multi', 'ANY'), -- 혼자/여럿
+	P_W ENUM ('play', 'watch', 'ANY'), -- 직접/관람
 	MV ENUM ('static', 'normal', 'dynamic'), -- 안뜀/적당히뜀/존나뜀
-	H_B ENUM ('healing', 'burning'), -- 힐링, 불태우기
-	RESV ENUM ('yes', 'no'), -- 예약 필수 여부
-	EQUIP ENUM('yes', 'no'), -- 장비 필요 유무
-	SD_F ENUM('self development', 'fun'), -- 자기개발인지 오락인지
-	NORM ENUM('normal', 'special') -- 이색/보편
+	H_B ENUM ('healing', 'burning', 'ANY'), -- 힐링, 불태우기
+	RESV ENUM ('yes', 'no', 'ANY'), -- 예약 필수 여부
+	EQUIP ENUM('yes', 'no', 'ANY'), -- 장비 필요 유무
+	SD_F ENUM('self development', 'fun', 'ANY'), -- 자기개발인지 오락인지
+	NORM ENUM('normal', 'special', 'ANY') -- 이색/보편
+);
+
+create table hobbiesimage
+(
+	image_path char(20),
+	image_address char(60),
+	foreign key (image_name) references hobbies (hobby_id)
 );
