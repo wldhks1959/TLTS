7eba2686729f650c6b13e8353febb946467cc079
pgjaeung <lju9176@naver.com>
2024-06-24 13:26:30 +0900

    sns login

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public', 'htm
 app.get('/get-main-hobbies', hobbyController.getMainHobbies);
 
 // 로그인 이후
-app.get('/main', userService.loginCheck, (req, res) => res.sendFile(path.join(__dirname, 'public', 'html', 'main.html')));
+app.get('/main',  (req, res) => res.sendFile(path.join(__dirname, 'public', 'html', 'main.html')));
 app.get('/modify', userService.loginCheck, (req, res) => res.sendFile(path.join(__dirname, 'public', 'html', 'modify.html')));
 app.get('/get-user_id', userService.loginCheck, userController.getUserId);
 app.get('/hobby', (req, res) => res.sendFile(path.join(__dirname, 'public', 'html', 'hobby.html')));
